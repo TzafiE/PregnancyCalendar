ad8578980f0be068505649501cc76d2e500616d1
TzafiE <tzafi.we>
2022-01-18 21:47:32 +0200

    Initialize repo on git

---
+++ server/routes/googleAuthentication.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.
+router.post('/', function(req, res, next) {
+    if(req.getHeader('X-Requested-With') === null) {
+        //ABORT
+    }
+
+    const
+
+});
+
+module.exports = router;
